992e16f2a844303e92d755250ae5813cd8873ed9
PricedTuna <edgarevegar04@gmail.com>
2024-07-06 18:24:57 -0700

    1

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,20 +1,23 @@
 import { Box } from "@mui/material";
 import Background from "../components/main/Background";
 import MainContainer from "../components/main/MainContainer";
+import AnimateViewWrapper from "../components/animation/AnimateViewWrapper";
 
 function MainLayout() {
   return (
-    <Background>
-      <Box
-        p={3}
-        bgcolor="rgba(0, 0, 0, 0.6)"
-        sx={{ height: "100vh", backdropFilter: "blur(20px)" }}
-      >
-        <Background>
-          <MainContainer />
-        </Background>
-      </Box>
-    </Background>
+    <AnimateViewWrapper>
+      <Background>
+        <Box
+          p={3}
+          bgcolor="rgba(0, 0, 0, 0.6)"
+          sx={{ height: "100vh", backdropFilter: "blur(20px)" }}
+        >
+          <Background>
+            <MainContainer />
+          </Background>
+        </Box>
+      </Background>
+    </AnimateViewWrapper>
   );
 }
 
