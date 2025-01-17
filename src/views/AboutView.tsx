0e2cb1a53e1ec1d2d5ab5fa388f21680a00cf847
PricedTuna <edgarevegar04@gmail.com>
2025-01-17 05:52:02 -0700

    update about view

--- src/views/AboutView.tsx
+++ src/views/AboutView.tsx
@@ -6,9 +6,9 @@ function AboutView() {
   return (
     <AnimateViewWrapper>
       <Box display={"flex"} flexDirection={"column"} gap={2} >
-        <PageText text="I love three things in this world, pixel art, pokemon games and solve problems." />
-        <PageText text="I'm Edgar Vega, a Mexican software engineer, I start my career at the 15yo learning algorythms with python, and now, I am a fullstack software developer, specialized in web and mobile with React and React Native in the front and NestJs and C# in the backend." />
-        <PageText text="This page has the purpose to expose all the projects where I have been worked, since I started until today, check them and tell me what do you think :p" />
+        <PageText text="I love three things in this world: pixel art, Pokémon games, and solving problems." />
+        <PageText text="I'm Edgar Vega, a Mexican software engineer. I started my career at 15, learning algorithms with Python. Now, I am a full-stack software developer, specializing in web and mobile development with React and React Native on the front end, and NestJS and C# on the back end." />
+        <PageText text="This page showcases all the projects I have worked on, from the beginning of my journey to today. Check them out and let me know what you think! :p" />
       </Box>
     </AnimateViewWrapper>
   );
